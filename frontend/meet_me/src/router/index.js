afaaeea3be542daca6e58e0c5fa72dcfe0e5561b
KANGMOONJEONG <image102@naver.com>
2024-07-24 18:52:08 +0900

    ㄹㅇㄴㄹ

--- frontend/meet_me/src/router/index.js
+++ frontend/meet_me/src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import TempView from '../views/TempView.vue'
 import MainPage from '../views/MainPage.vue'
+import TempView from '../views/TempView.vue'
 
 const routes = [
   {
@@ -22,14 +22,12 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-<<<<<<< HEAD
   },
   {
     path: '/tempview',
     name: 'TempView',
     component: TempView
   },
->>>>>>>>> Temporary merge branch 2
 
 ]
 
