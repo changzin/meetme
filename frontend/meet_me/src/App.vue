244058a55c6d643963823f2983e1da0b365e8f43
sgy0917 <sgy0917@naver.com>
2024-07-24 19:07:08 +0900

    login,회원가입제작

--- frontend/meet_me/src/App.vue
+++ frontend/meet_me/src/App.vue
@@ -27,4 +27,6 @@ nav a {
 nav a.router-link-exact-active {
   color: #42b983;
 }
+
+
 </style>
--- frontend/meet_me/src/router/index.js
+++ frontend/meet_me/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import MainPage from '../views/MainPage.vue'
+import userLogin from '../views/nouser/userLogin.vue'
 
 const routes = [
   {
@@ -21,6 +22,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path:'/login',
+    name:'userLogin',
+    component: userLogin
   }
 
 ]
