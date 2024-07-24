3d7ea70115f60f6457cb1f61d6ba92d800badd8c
changzin <changzin0126@gmail.com>
2024-07-24 19:06:06 +0900

    관리자 회원관리 css 작성 중

--- frontend/meet_me/src/router/index.js
+++ frontend/meet_me/src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Member from '../views/admin/Member.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/admin/member',
+    name: 'member',
+    component: Member
   },
 ]
 
