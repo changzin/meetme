06390ab7ca4fb475435d87aba7a0548b62995636
changzin <changzin0126@gmail.com>
2024-07-25 11:15:50 +0900

    관리자 회원관리 페이지 작업 완료

--- frontend/meet_me/src/views/admin/Member.vue
+++ frontend/meet_me/src/views/admin/Member.vue
@@ -1,74 +1,82 @@
 <template>
-<div>
-    <div class="admin_header">회원 관리</div>
-    <div class="admin_search_div">
-        <form class="admin_search_form">
-            <img src="/icon/admin/search.svg" class="search_icon">
-            <input type="text" class="search_input" placeholder="이메일, 닉네임으로 검색">
-        </form>
-    </div>
-    <div class="admin_table_div">
-        <table class="admin_table">
-            <thead>
-                <tr class="admin_table_thead">
-                    <td>번호</td>
-                    <td>이메일</td>
-                    <td>닉네임</td>
-                    <td>총 결제금액</td>
-                    <td>신고</td>
-                    <td>차단</td>
-                </tr>
-            </thead>
-            <tbody>
-                <tr class="tr_gray">
-                    <td>1</td>
-                    <td>sa123@naver.com</td>
-                    <td>nickname1</td>
-                    <td>10,000,000</td>
-                    <td>2</td>
-                    <td>
-                        <div class="block_buttons">
-                            <div class="block_button_active">Y</div>
-                            <div class="block_button_inactive">N</div>
-                        </div>
-                    </td>
-                </tr>
-                <tr class="tr_white">
-                    <td>번호</td>
-                    <td>이메일</td>
-                    <td>닉네임</td>
-                    <td>총 결제금액</td>
-                    <td>신고</td>
-                    <td>
-                        <div class="block_buttons">
-                            <div class="block_button_active">Y</div>
-                            <div class="block_button_inactive">N</div>
-                        </div>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
-    <div class="admin_pagination_box">
-        <div class="pagination_button_active">
-            1
-        </div>
-        <div class="pagination_button_inactive">
-            1
-        </div>
-        <div class="pagination_button_inactive">
-            1
-        </div>
-        <div class="pagination_button_inactive">
-            1
-        </div>
-        <div class="pagination_button_inactive">
-            1
+<div class="web_body">
+    <AdminHeader />
+    <div class="admin_web_inner_flex">
+        <div>
+            <div class="admin_header_text">회원 관리</div>
+            <div class="admin_search_div">
+                <form class="admin_search_form">
+                    <img src="/icon/admin/search.svg" class="search_icon">
+                    <input type="text" class="search_input" placeholder="이메일, 닉네임으로 검색">
+                </form>
+            </div>
+            <div class="admin_table_div">
+                <table class="admin_table">
+                    <thead>
+                        <tr class="admin_table_thead">
+                            <td>번호</td>
+                            <td>이메일</td>
+                            <td>닉네임</td>
+                            <td>총 결제금액</td>
+                            <td>신고</td>
+                            <td>차단</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class="tr_gray">
+                            <td>1</td>
+                            <td>sa123@naver.com</td>
+                            <td>nickname1</td>
+                            <td>10,000,000</td>
+                            <td>2</td>
+                            <td>
+                                <div class="block_buttons">
+                                    <div class="block_button_active">Y</div>
+                                    <div class="block_button_inactive">N</div>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr class="tr_white">
+                            <td>번호</td>
+                            <td>이메일</td>
+                            <td>닉네임</td>
+                            <td>총 결제금액</td>
+                            <td>신고</td>
+                            <td>
+                                <div class="block_buttons">
+                                    <div class="block_button_active">Y</div>
+                                    <div class="block_button_inactive">N</div>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="admin_web_inner_flex">
+                <div class="admin_pagination_box">
+                    <div class="pagination_button_active">
+                        1
+                    </div>
+                    <div class="pagination_button_inactive">
+                        1
+                    </div>
+                    <div class="pagination_button_inactive">
+                        1
+                    </div>
+                    <div class="pagination_button_inactive">
+                        1
+                    </div>
+                    <div class="pagination_button_inactive">
+                        1
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
 </template>    
 <script>
+
 export default {
     data() {
         return {
@@ -105,8 +113,10 @@ tbody tr:hover{
     background-color: #5C09E3;
     color: #FFFFFF;
     width:30px;
-    height:22px;
-    padding-top:8px;
+    height:30px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
     border-radius: 7px;
     font-size:13px;
 }
@@ -115,8 +125,10 @@ tbody tr:hover{
     background-color: #D9D9D9;
     color: #FFFFFF;
     width:30px;
-    height:22px;
-    padding-top:8px;
+    height:30px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
     border-radius: 7px;
     font-size:13px;
 }
@@ -125,20 +137,24 @@ tbody tr:hover{
     background-color: #5C09E3;
     color: #FFFFFF;
     width:40px;
-    height:17px;
-    border-radius: 7px;
+    height:21px;
+    border-radius: 5px;
     font-size:13px;
-    padding-top:4px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 }
 
 .block_button_inactive{
     background-color: #D9D9D9;
     color: #FFFFFF;
     width:40px;
-    height:17px;
-    border-radius: 7px;
+    height:21px;
+    border-radius: 5px;
     font-size:13px;
-    padding-top:4px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 }
 
 .block_buttons{
@@ -180,5 +196,4 @@ tbody :last-child :first-child{
 tbody :last-child :last-child{
     border-bottom-right-radius: 8px;
 }
-
 </style>
