3e49553b4b36585401974925a2af8f4a26b10b4a
miru001015 <miru001015@gmail.com>
2024-07-31 19:36:09 +0900

    recommend

--- backend/meet_me/router/index.js
+++ backend/meet_me/router/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const userRouter = require('./userRouter');
+const recommendRouter = require('./recommendRouter');
 
 const router = express.Router();
 
 // 여러 라우터 파일을 모아서 묶어주는 역할을 수행
 router.use("/user", userRouter);
+router.use("/recommend", recommendRouter);
 
 module.exports = router;
