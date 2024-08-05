e4801039f71f64a68ca33eadc52aa277eb58d6b7
sgy0917 <sgy0917@naver.com>
2024-08-05 18:06:04 +0900

    userFeature 작업 끝?

--- backend/meet_me/controller/userFeatureController.js
+++ backend/meet_me/controller/userFeatureController.js
@@ -48,26 +48,31 @@ exports.FeatureInsert = async (req,res) =>{
   let query = '';
   let result = [];
   let responseBody = {};
-  const user_id = req.body.user_id
-  const user_feature_id = req.body.user_feature_id;
+  let user_id = req.body.user_id
+  let user_feature_ids = req.body.user_feature_id;
+  let user_feature_id 
     console.log(user_id)
-    console.log(user_feature_id);
+    console.log(user_feature_ids);
 
+    
   try{
       await conn.beginTransaction();
       //user 없어서 대충 넣기 
-      query = "INSERT INTO user_feature_bridge (user_id,user_feature_id) VALUES (?,?);"
-      result = await db(conn,query[user_id,user_feature_id]);
-      
-      console.log(result);
 
-      const responseBody = {
-          status : 200,
-          featureIn : result,
-          message : "userFeature 조회 성공"
-      }
-      await conn.commit();
-      res.status(200).json(responseBody);
+        for(let i=0; i< user_feature_ids.length; i++){
+             user_feature_id = user_feature_ids[i]; 
+            query = `INSERT INTO user_feature_bridge (user_id,user_feature_id) VALUES (?,?)`;
+            result = await db(conn,query,[user_id,user_feature_id]);
+        }
+        
+
+        const responseBody = {
+            status : 200,
+            featureIn : result,
+            message : "userfeature 인서트 성공"
+        }
+        await conn.commit();
+        res.status(200).json(responseBody);
   }   
   catch(err){
       console.error(err);
