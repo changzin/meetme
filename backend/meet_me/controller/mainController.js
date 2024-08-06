e39565704127ff790b0b0c9424b9063845e950ad
miru001015 <miru001015@gmail.com>
2024-08-06 13:50:07 +0900

    recommendList 수정

--- backend/meet_me/controller/mainController.js
+++ backend/meet_me/controller/mainController.js
@@ -8,13 +8,29 @@ exports.main = async(req, res)=>{
         let result = [];
         let responseBody = {};
         
-        query = ``;
-        result = await db(conn, query);
+        query = `SET @logged_in_user_id = ?`;
+        results = await db(conn, query, [3]); //나중에 로그인한 유저 변수 지정해줘야함
         
-        query = ``;
-        results = await db(conn, query, [user_id1]);
+        query = `DROP TEMPORARY TABLE IF EXISTS temp`;
+        results = await db(conn, query);
         
-        query = ``;
+        query = `CREATE TEMPORARY TABLE temp AS
+                SELECT user_id, user_nickname, user_age 
+                FROM meet_me.user
+                WHERE user_id != @logged_in_user_id
+                ORDER BY RAND()
+                LIMIT 10`;
+        results = await db(conn, query); //성별 구분 확인 , 차단 여부 확인
+
+        query = `SELECT * FROM temp`;
+        results = await db(conn, query);
+
+        query = `SELECT ufb.user_feature_bridge_id, uf.user_feature_value, u1.user_id
+                FROM user_feature_bridge ufb
+                JOIN user u1 ON ufb.user_id = u1.user_id
+                JOIN user_feature uf ON ufb.user_feature_id = uf.user_feature_id
+                WHERE u1.user_id = ?
+                LIMIT 10`;
 
         for(let i = 0; i < results.length; i++) {
             let user_id = results[i].user_id;
@@ -32,7 +48,8 @@ exports.main = async(req, res)=>{
         responseBody = {
             status: 200,
             recommendList: result,
-            results : results
+            results : results,
+            // featureList : featureList,
         };
         await conn.commit();
         res.status(200).json(responseBody);
--- backend/meet_me/router/recommendRouter.js
+++ backend/meet_me/router/recommendRouter.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { recommend } = require('../controller/recommendController.js');
+const { recommend , heart , sendMatching} = require('../controller/recommendController.js');
 
 const router = express.Router();
 
 // 컨트롤러의 여러 함수들을 Request URI에 맞게 매핑하는 역할 수행
 router.post('/list', recommend);
+router.post('/heart', heart);
+router.post('/sendmatching', sendMatching);
 
 module.exports = router;
