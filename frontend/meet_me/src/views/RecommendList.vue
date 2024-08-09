271193149736152d1fb8651a078c45934b664ade
miru001015 <miru001015@gmail.com>
2024-08-09 11:06:58 +0900

    수정

--- frontend/meet_me/src/views/RecommendList.vue
+++ frontend/meet_me/src/views/RecommendList.vue
@@ -160,15 +160,11 @@ export default {
                 console.error(err);
             }
         },
-        async sendMatching(user_id2){
-            
-            this.isImageTwoVisible2 = !this.isImageTwoVisible2;
-            
-            
-            try{
-
-                await this.$api(`/recommend/sendmatching`, {user_id: 1, user_id2}, "POST");
-            }catch(err){
+        async sendMatching(user_id2) {
+            try {
+                await this.$api(`/recommend/sendmatching`, { access_token: this.$getAccessToken(), user_id2 }, "POST");
+                await this.getRecommendList();
+            } catch (err) {
                 console.error(err);
             }
         },
